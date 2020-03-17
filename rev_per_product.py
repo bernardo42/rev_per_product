21f6cfcf8008f15c239a3475f372144cdc457965
bernardoandreypanggabean <bernardoandreypanggabean@gmail.com>
2020-03-17 11:57:17 +0700

    fixed

--- rev_per_product.py
+++ rev_per_product.py
@@ -18,5 +18,4 @@ def rev_per_product():
         return Response(df, mimetype='application/json')
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 33507))
-    app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0',port=5000)
